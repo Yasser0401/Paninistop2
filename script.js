f4391e6c5cef69a619b08faaae8755d74a0aa144
Yasser0401 <yasserbkt270@gmail.com>
2024-11-16 09:54:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript pour des fonctionnalités supplémentaires (par exemple : notifications)
+document.querySelector('.reservation-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Merci pour votre réservation ! Nous vous contacterons bientôt.');
+});
